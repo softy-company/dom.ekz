e9092e496ee551ea4c9e4448501a7e9e63144ae8
softy-company <vkserfingazi@gmail.com>
2023-11-25 19:42:08 +0600

    d

--- script.js
+++ script.js
@@ -52,9 +52,9 @@ createBtn.addEventListener("click", () => {
   add();
 
 
-  if(nameInput.value.trim() === "" || emailInput.value.trim() === ""|| imgInput.value.trim() === "" ) {
-    alert("Please enter")
-  }
+//   if(nameInput.value.trim() === "" || emailInput.value.trim() === ""|| imgInput.value.trim() === "" ) {
+//     alert("Please enter")
+//   }
 });
 
 
